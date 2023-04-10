5ebc3e02eff014c8bfabb5330097777f142ef5c0
Masih <said0983@stud.kea.dk>
2023-04-10 14:56:02 +0200

    Added to DBRepository and services

--- src/main/java/com/example/wishlist/repositories/DBRepository.java
+++ src/main/java/com/example/wishlist/repositories/DBRepository.java
@@ -11,10 +11,7 @@ public class DBRepository {
 
     private final List<User> users = new ArrayList<>();
 
-    public List<User> getSuperheroes() {
-        if (!users.isEmpty()){
-            return users;
-        }
+    public List<User> getUsers() {
         try (Connection con = DBManager.getConnection()) {
             String SQL = "SELECT * FROM wishlistDatabase.user;";
             Statement stmt = con.createStatement();
@@ -32,18 +29,18 @@ public class DBRepository {
         return users;
     }
 
-    public User fetchUser(String username) {
+    public User fetchUser(int ID) {
         try(Connection con = DBManager.getConnection()) {
-            String SQL = "SELECT * FROM user WHERE UserName = ?;";
+            String SQL = "SELECT * FROM user WHERE userID = ?;";
             PreparedStatement pstmt = con.prepareStatement(SQL);
-            pstmt.setString(1, username);
+            pstmt.setInt(1, ID);
             ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
                 int userID = rs.getInt("UserID");
-                String usernametmp = rs.getString("Username");
+                String usernamevar = rs.getString("Username");
                 String password = rs.getString("Password");
                 String email = rs.getString("Email");
-                return new User(userID, usernametmp, password, email);
+                return new User(userID, usernamevar, password, email);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -51,44 +48,57 @@ public class DBRepository {
         return null;
     }
 
-    public List<Wishlist> getWishlists(String username) {
+
+    public List<Wishlist> getWishlists() {
         try(Connection con = DBManager.getConnection()) {
-            String SQL = "SELECT * FROM user JOIN wishlist WHERE UserID = WishlistID AND UserName = ?;";
+            String SQL = "SELECT * FROM user JOIN wishlist WHERE user.userID = wishlistID;";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(SQL);
+            while (rs.next()) {
+                int wishlistID = rs.getInt("wishlistID");
+                String wishlistNamevar = rs.getString("wishlistName");
+                int userID = rs.getInt("userID");
+                Wishlist wishlist = new Wishlist(wishlistID, wishlistNamevar);
+                fetchUser(userID).addWishlist(wishlist);
+                return fetchUser(userID).getWishlists();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public Wishlist fetchWishlist(int wishlistID) {
+        try(Connection con = DBManager.getConnection()) {
+            String SQL = "SELECT * FROM wishlist WHERE wishlistID = ?;";
             PreparedStatement pstmt = con.prepareStatement(SQL);
-            pstmt.setString(1, username);
+            pstmt.setInt(1, wishlistID);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                int wishlistIDvar = rs.getInt("wishlistID");
+                String wishlistname = rs.getString("wishlistName");
+                return new Wishlist(wishlistIDvar, wishlistname);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+
+    public List<Item> getItemlist(String itemName, int ID) {
+        try(Connection con = DBManager.getConnection()) {
+            Wishlist wishlist = fetchWishlist(ID);
+            String SQL = "SELECT * FROM wishlist JOIN item WHERE wishlistID = itemID AND itemName = ?;";
+            PreparedStatement pstmt = con.prepareStatement(SQL);
+            pstmt.setString(1, itemName);
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
-                int userID = rs.getInt("UserID");
-                String usernametmp = rs.getString("Username");
-                String password = rs.getString("Password");
-                String email = rs.getString("Email");
-                User user = new User(userID, usernametmp, password, email);
-                return user.getWishlists();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return null;
-    }
-
-    public void addWishlist(User user, Wishlist wishlist) {
-        user.addWishlist(wishlist);
-    }
-
-    //Skal ændres, har ikke fikset hvordan items list og wish lists skal tilknyttes user.
-    public List<Item> getItemlist(String wishlistName) {
-        ArrayList<Item> items = new ArrayList<>();
-        try(Connection con = DBManager.getConnection()) {
-            String SQL = "SELECT * FROM wishlist JOIN item WHERE WishlistID = ItemID AND WishlistName = ?;";
-            PreparedStatement pstmt = con.prepareStatement(SQL);
-            pstmt.setString(1, wishlistName);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
                 int itemID = rs.getInt("ItemID");
-                String itemName = rs.getString("ItemName");
-                Item item = new Item(itemID, itemName);
-                items.add(item);
-                return items;
+                String itemNamevar = rs.getString("ItemName");
+                Item item = new Item(itemID, itemNamevar);
+                wishlist.addItem(item);
+                return wishlist.getItems();
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
--- src/main/java/com/example/wishlist/services/UserServices.java
+++ src/main/java/com/example/wishlist/services/UserServices.java
@@ -1,7 +1,26 @@
 package com.example.wishlist.services;
 
+import com.example.wishlist.model.User;
+import com.example.wishlist.repositories.DBRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServices {
+
+    DBRepository dbRepository;
+
+    public UserServices(DBRepository dbRepository){
+        this.dbRepository = dbRepository;
+    }
+
+    public List<User> getUsers() {
+        return dbRepository.getUsers();
+    }
+
+    public User fetchUser(int userID) {
+        return dbRepository.fetchUser(userID);
+    }
+
 }

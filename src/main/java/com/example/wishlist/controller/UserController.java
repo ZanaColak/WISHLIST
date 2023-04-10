664f2001e0126481256431ea63b03b31dd2df664
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2023-04-10 23:18:43 +0200

    Changed the html files and something in the controller class

--- src/main/java/com/example/wishlist/controller/UserController.java
+++ src/main/java/com/example/wishlist/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.example.wishlist.controller;
 
+import com.example.wishlist.model.Item;
 import com.example.wishlist.model.User;
 import com.example.wishlist.repositories.DBRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,12 +25,16 @@ public class UserController {
         model.addAttribute("user", user);
         return "index";
     }
-
     @GetMapping("/user/signup")
     public String submitForm(@ModelAttribute("user") User user){
         System.out.println(user);
         return "signup";
-
+    }
+    @GetMapping("/user/product")
+    public String submitProduct(Model model){
+        Item item = new Item();
+        model.addAttribute("item", item);
+        return "product_form";
     }
 
 }
--- src/main/java/com/example/wishlist/model/Item.java
+++ src/main/java/com/example/wishlist/model/Item.java
@@ -10,6 +10,10 @@ public class Item {
         this.name = name;
     }
 
+    public Item() {
+
+    }
+
     public String getName() {
         return name;
     }
